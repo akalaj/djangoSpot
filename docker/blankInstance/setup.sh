a69570eed57a9d9481e75e1cba26aa17361e6fb8
Alexander Kalaj <adkalaj@gmail.com>
2019-09-02 17:24:46 -0400

    Adding fully functional polls app to docker folder

---
+++ docker/blankInstance/setup.sh
@@ -0,0 +1,34 @@
+###Temp url
+
+#Run below command as root
+# curl -sS https://raw.githubusercontent.com/akalaj/djangoSpot/master/docker/setup.sh
+
+###Install dependencies
+yum install vim git yum-utils -y
+
+####Install docker
+
+#add repos
+yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+
+#install docker
+yum install docker-ce -y
+
+#install docker-compose
+echo -e "Installing docker-compose"
+curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+chmod 777 /usr/local/bin/docker-compose
+ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+
+###Retrieve files
+
+#Git clone files
+echo -e "Retrieving files from Github"
+git clone https://github.com/akalaj/djangoSpot.git
+
+###Starting Docker
+systemctl start docker
+
+#Completion notice
+echo -e "Please run the following commands:\n\ndocker-compose run web django-admin startproject composeexample .\n\ndocker-compose up"
